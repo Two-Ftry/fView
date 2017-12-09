7894649e4fa98b5c24dc345314a73036eafde1e6
jianfeng_huang <jianfeng_huang@kingdee.com>
2017-12-09 17:06:59 +0800

    sdfsdf

---
+++ src/test.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: '',
+    data () {
+    },
+    components: {
+    }
+}
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+
+</style>
